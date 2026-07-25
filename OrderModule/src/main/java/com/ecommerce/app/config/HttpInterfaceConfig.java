fd62fecad7bbd80232f1893ed1c73ffd0c35280b
Ishon Panda <pandaishon@gmail.com>
2026-07-26 02:01:29 +0530

    fixed /refresh -> eureka anti pattern, gateway configs added

--- OrderModule/src/main/java/com/ecommerce/app/config/HttpInterfaceConfig.java
+++ OrderModule/src/main/java/com/ecommerce/app/config/HttpInterfaceConfig.java
@@ -1,11 +1,16 @@
 package com.ecommerce.app.config;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -15,11 +20,38 @@ import java.util.Optional;
 @Configuration
 public class HttpInterfaceConfig {
 
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+    @Autowired(required = false)
+    private Tracer tracer;
+    @Autowired(required = false)
+    private Propagator propagator;
+
     @Bean
     @LoadBalanced
     public RestClient.Builder getRestClientBuilderLB()
     {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+        if(observationRegistry!=null)
+        {
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+        return builder;
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return ((request, body, execution) ->{
+            if(tracer!=null && propagator!=null && tracer.currentSpan()!=null)
+            {
+                propagator.inject(tracer.currentTraceContext().context(),
+                        request.getHeaders(),
+                        ((carrier, key, value) -> {
+                            carrier.add(key,value);
+                        }));
+            }
+            return execution.execute(request,body);
+        } );
+
     }
 
     @Bean
@@ -59,5 +91,6 @@ public class HttpInterfaceConfig {
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(UserServiceClient.class);
     }
+
 
 }
